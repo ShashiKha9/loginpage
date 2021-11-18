1babf3a788c2aba3c6d833ad02e2e35b87934442
ashish kha <ashish.kha@monstar-lab.com>
2021-11-19 00:26:15 +0530

    otp screen receive done

--- lib/Pages/otp_screen.dart
+++ lib/Pages/otp_screen.dart
@@ -1,136 +1,150 @@
 import 'package:firebase_auth/firebase_auth.dart'as _auth;
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pinput/pin_put/pin_put.dart';
 
 import 'homepgae_screen.dart';
+ enum MobileVerificationState{
+  Show_Mobile_Form_State,
+  Show_Otp_Form_State,
+
+}
 
 class OtpScreen extends StatefulWidget {
 @override
   OtpScreenState createState() => OtpScreenState();
 }
 class OtpScreenState extends State<OtpScreen>{
+   MobileVerificationState currentState=MobileVerificationState.Show_Mobile_Form_State;
   final  GlobalKey<ScaffoldState> _scaffoldkey =GlobalKey<ScaffoldState>();
-  late String _verificationId;
-  final TextEditingController _phoneNumber = TextEditingController();
-  final TextEditingController _pinPutController = TextEditingController();
-  final FocusNode _pinPutFocusNode = FocusNode();
-  final BoxDecoration pinPutDecoration = BoxDecoration(
-    color: const Color.fromRGBO(43, 46, 66, 1),
-    borderRadius: BorderRadius.circular(10.0),
-    border: Border.all(
-      color: const Color.fromRGBO(126, 203, 224, 1),
-    ),
-  );
+   late String verificationId;
+  static bool  showLoading= false;
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController otpController = TextEditingController();
+  FirebaseAuth _auth =FirebaseAuth.instance;
+
+   Future<void> signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) async {
+     setState(() {
+       showLoading= true;
+     });
+     try {
+       final authCredential = await _auth.signInWithCredential(
+           phoneAuthCredential);
+       setState(() {
+         showLoading= true;
+       });
+       if(authCredential?.user!= null){
+         Navigator.push(context, MaterialPageRoute(builder: (context)=> Home()));
+       }
+     } on FirebaseAuthException catch(e){
+       setState(() {
+         showLoading=false;
+       });
+
+       _scaffoldkey.currentState!.showSnackBar(SnackBar(content: Text(e.message!)));
+
+     }
+   }
+
+
+getMobileFormWidget(context){
+    return Column(
+      children: [
+        Padding(padding: EdgeInsets.only(top: 90),
+      child:  TextField(
+          controller: phoneNumberController,
+          decoration: InputDecoration(
+            hintText: "Phone Number"
+          ),
+        ),
+        ),
+        SizedBox(height: 16,),
+        FlatButton(onPressed: () async{
+          setState(() {
+            showLoading=true;
+          });
+         await _auth.verifyPhoneNumber(phoneNumber: phoneNumberController.text,
+
+              verificationCompleted: (phoneAuthCredential)async{
+                setState(() {
+                  showLoading=false;
+                });
+
+              },
+              verificationFailed: (verificationFailed)async {
+                setState(() {
+                  showLoading=false;
+                });
+                _scaffoldkey.currentState!.showSnackBar(SnackBar(content: Text(verificationFailed.message!)));
+
+              },
+              codeSent: (verificationId,resendingToken)async{
+            setState(() {
+              currentState=MobileVerificationState.Show_Otp_Form_State;
+              this.verificationId=verificationId;
+              showLoading= false;
+            });
+
+              },
+           timeout: Duration(seconds: 40),
+              codeAutoRetrievalTimeout: (verificationId)async {
+                this.verificationId=verificationId;
+           showLoading=false;
+
+              },
+
+          );
+        }
+        , child: Text("SEND")),
+      ],
+    );
+
+  }
+  getOtpFormWidget(context){
+    return Column(
+      children: [
+        Padding(padding: EdgeInsets.only(top: 90),
+       child: TextField(
+          controller: otpController,
+          decoration: InputDecoration(
+              hintText: "Enter OTP"
+          ),
+        ),
+        ),
+        SizedBox(height: 16,),
+        FlatButton(onPressed: () {
+          PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+              verificationId: verificationId, smsCode: otpController.text,);
+          signInWithPhoneAuthCredential(phoneAuthCredential);
+        }
+            , child: Text("VERIFY")),
+
+
+      ],
+    );
+
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldkey,
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.only(top: 30),
-       child:   Text("OTP Verification",style: TextStyle(fontSize: 22,fontWeight: FontWeight.w700),)
-          ),
-          TextFormField(
-              keyboardType: TextInputType.phone,
-              controller: _phoneNumber,
-              decoration: InputDecoration(
-                labelText: "Enter Phone",
-                contentPadding: EdgeInsets.symmetric(
-                    vertical: 15.0, horizontal: 10.0),
-              )),
-          Container(
-            child: RaisedButton(onPressed: (){
-              verifyPhone();
-            },
-              child: Text("Send OTP"),
-            ),
-          ),
-
-    Padding(
-    padding: const EdgeInsets.all(30.0),
-    child: PinPut(
-    fieldsCount: 5,
-    textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
-    eachFieldWidth: 40.0,
-    eachFieldHeight: 55.0,
-    focusNode: _pinPutFocusNode,
-    controller: _pinPutController,
-    submittedFieldDecoration: pinPutDecoration,
-    selectedFieldDecoration: pinPutDecoration,
-    followingFieldDecoration: pinPutDecoration,
-    pinAnimationType: PinAnimationType.fade,
-    onSubmit: (pin) async {
-      try{
-    await FirebaseAuth.instance.signInWithCredential(
-    PhoneAuthProvider.credential(verificationId: _verificationId, smsCode: pin))
-        .then((value) async{
-    if(value.user != null){
-Navigator.pushAndRemoveUntil(context, MaterialPageRoute
-(builder: (context)=>Home()), (route) => true) ;   }
-    });
-    } catch(e){
-        FocusScope.of(context).unfocus();
-        _scaffoldkey.currentState!.showSnackBar(SnackBar(content: Text("Invalid OTP")));
-    }
-
-    },
-    ),
-    ),
-          RaisedButton(onPressed: (){
-            
-          },
-          child: Text("Submit"),)
-        ],
-      ),
-
+      body: Container(
+        child: showLoading ? Center(
+          child: CircularProgressIndicator(),
+        ) :currentState==MobileVerificationState.Show_Mobile_Form_State
+       ? getMobileFormWidget(context)
+       : getOtpFormWidget(context),
+      )
     );
-  }
-
-  verifyPhone() async {
-    PhoneVerificationCompleted verificationCompleted =
-        (PhoneAuthCredential phoneAuthCredential) async {
-      await _auth.FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
-      showSnackbar("Phone number automatically verified and user signed in: ${_auth.FirebaseAuth.instance.currentUser!.uid}");
-    };
-    PhoneVerificationFailed verificationFailed =
-        (FirebaseAuthException authException) {
-      showSnackbar('Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
-    };
-    PhoneCodeSent codeSent =
-        (String verificationId,int? forceResendingToken  ) async {
-      showSnackbar('Please check your phone for the verification code.');
-      _verificationId = verificationId;
-    } as PhoneCodeSent;
-    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
-        (String verificationId) {
-      showSnackbar("verification code: " + verificationId);
-      _verificationId = verificationId;
-    };
-
-    try {
-      await _auth.FirebaseAuth.instance.verifyPhoneNumber(
-          phoneNumber: _phoneNumber.text,
-          timeout: const Duration(seconds: 5),
-          verificationCompleted: verificationCompleted,
-          verificationFailed: verificationFailed,
-          codeSent: codeSent,
-          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
-    } catch (e) {
-      showSnackbar("Failed to Verify Phone Number: ${e}");
-    }
-  }
-  void showSnackbar(String message) {
-    _scaffoldkey.currentState?.showSnackBar(SnackBar(content: Text(message)));
-  }
 
 
-  @override
-  void initState(){
-    super.initState();
-    verifyPhone();
-  }
+
+
+
 }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:loginpage/Pages/loginpage_screen.dart';
 import 'package:loginpage/Pages/otp_screen.dart';
 
 void main() async {
