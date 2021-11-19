fec10be6a1bb34b6379d01a529db143e43083428
ashish kha <ashish.kha@monstar-lab.com>
2021-11-19 20:58:25 +0530

    resend button done

--- lib/Pages/otp_screen.dart
+++ lib/Pages/otp_screen.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart'as _auth;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pinput/pin_put/pin_put.dart';
 
 import 'homepgae_screen.dart';
  enum MobileVerificationState{
@@ -18,7 +21,9 @@ class OtpScreen extends StatefulWidget {
   OtpScreenState createState() => OtpScreenState();
 }
 class OtpScreenState extends State<OtpScreen>{
-   MobileVerificationState currentState=MobileVerificationState.Show_Mobile_Form_State;
+
+  MobileVerificationState currentState=MobileVerificationState.Show_Mobile_Form_State;
+  late String dialCodeDigits = "+00";
   final  GlobalKey<ScaffoldState> _scaffoldkey =GlobalKey<ScaffoldState>();
    late String verificationId;
   static bool  showLoading= false;
@@ -48,21 +53,47 @@ class OtpScreenState extends State<OtpScreen>{
 
      }
    }
+   void starttimer(){
+     const onsec= Duration(seconds: 1);
+     Timer timer=Timer.periodic(onsec,(timer){
+       if(start==0){
+         setState(() {
+           timer.cancel();
+           wait=false;
+         });
+
+       }else{
+         setState(() {
+           start--;
+
+         });
+       }
+
+     });
+
+   }
 
 
 getMobileFormWidget(context){
     return Column(
       children: [
         Padding(padding: EdgeInsets.only(top: 90),
+      child:Container(
       child:  TextField(
           controller: phoneNumberController,
           decoration: InputDecoration(
-            hintText: "Phone Number"
+            hintText: "Phone Number",
+            prefixIcon: Padding(padding: EdgeInsets.symmetric(vertical: 14,horizontal: 15),
+  child:Text("(+91)",style: TextStyle(fontSize: 16),)
+            )
           ),
+
+        ),
         ),
         ),
         SizedBox(height: 16,),
         FlatButton(onPressed: () async{
+          starttimer();
           setState(() {
             showLoading=true;
           });
@@ -70,6 +101,7 @@ getMobileFormWidget(context){
 
               verificationCompleted: (phoneAuthCredential)async{
                 setState(() {
+
                   showLoading=false;
                 });
 
@@ -89,16 +121,17 @@ getMobileFormWidget(context){
             });
 
               },
-           timeout: Duration(seconds: 40),
               codeAutoRetrievalTimeout: (verificationId)async {
-                this.verificationId=verificationId;
-           showLoading=false;
 
               },
 
           );
+
         }
-        , child: Text("SEND")),
+
+        ,
+          child: Text("SEND"),
+        ),
       ],
     );
 
@@ -120,15 +153,59 @@ getMobileFormWidget(context){
               verificationId: verificationId, smsCode: otpController.text,);
           signInWithPhoneAuthCredential(phoneAuthCredential);
         }
-            , child: Text("VERIFY")),
+            , child: Text("Get Started")),
+       Wrap(
+         children: [
+           Text("Send OTP again in ",style: TextStyle(
+             fontSize: 14
+           ),),
+           Text("00:$start ",style: TextStyle(
+               fontSize: 14,color: Colors.red
+           ),),
+           Text("sec ",style: TextStyle(
+               fontSize: 14
+           ),),
+    ]
+       ),
+        SizedBox(
+          height: 10,
+        ),
 
 
-      ],
+    RichText(text: TextSpan(
+    children: [
+    TextSpan(
+    text: "Did not get otp",
+    style: TextStyle(fontSize: 14,color: Colors.black)
+    ),
+    TextSpan(
+    text: " resend?",
+    style: TextStyle(fontSize: 16,color: wait? Colors.blue:Colors.grey),
+    recognizer: TapGestureRecognizer()
+    ..onTap=wait ?null:(){
+      setState(() {
+        start=30;
+        start--;
+        wait= true;
+      });
+    print("judnu");
+    }
+
+    )
+    ]
+    ))
+    ],
+
+
+
+
+
     );
 
   }
 
-
+int start= 45;
+   bool wait = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
